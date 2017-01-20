2f0b9732076e6fc259a54d726ae08f0715dd1893
marcsnts <msanto03@mail.uoguelph.ca>
2017-01-20 11:35:55 -0500

    Initial commit

---
+++ AddressBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AddressBook
+//
+//  Created by Marc Santos on 2017-01-20.
+//  Copyright © 2017 Marc Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
